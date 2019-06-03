32a06750bf1506374ea81d97b0042473261fa5b1
Hong Hung <lhhunghimself@gmail.com>
2019-06-03 11:26:23 -0700

    Added profiler build Updated build and Dockerfiles

--- scripts/build_profiler.sh
+++ scripts/build_profiler.sh
@@ -2,5 +2,5 @@
 
 sudo docker build -t rnaseq-umi-cpp -f Dockerfile.build  ${PWD}
 sudo docker run --rm -v ${PWD}:/local rnaseq-umi-cpp /bin/sh -c "cp -r source/w* /local/. && cp /usr/local/bin/bwa /local/."
-sudo docker build -t biodepot/rnaseq-umi-cpp  -f Dockerfile  ${PWD}
+sudo docker build -t biodepot/rnaseq-umi-cpp:profiler  -f Dockerfile-profiler  ${PWD}
 sudo rm -rf w96 w384
